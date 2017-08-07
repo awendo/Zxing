f18e43c0bd8018043960edb96c258bd9b53abd5f
awendo <hongsheng9311@163.com>
2017-08-07 15:17:48 +0800

    Initial commit

---
+++ app/src/main/java/com/awen/zxing/MainActivity.java
@@ -0,0 +1,27 @@
+package com.awen.zxing;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        IntentIntegrator integrator = new IntentIntegrator(this);
+        integrator.setPrompt("请扫描"); //底部的提示文字，设为""可以置空
+        integrator.setCameraId(0); //前置或者后置摄像头
+        integrator.setBeepEnabled(false); //扫描成功的「哔哔」声，默认开启
+        integrator.initiateScan();
+        integrator.setCaptureActivity(ScanActivity.class);
+
+    }
+
+}
---
+++ app/src/main/java/com/awen/zxing/ScanActivity.java
@@ -0,0 +1,33 @@
+package com.awen.zxing;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+import com.journeyapps.barcodescanner.CaptureActivity;
+
+public class ScanActivity extends CaptureActivity {
+    private static final String TAG = "ScanActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if(result != null) {
+            if(result.getContents() == null) {
+                Log.d(TAG, "Cancelled");
+                Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show();
+            } else {
+                Log.d(TAG, "Scanned: " + result.getContents());
+                Toast.makeText(this, "Scanned: " + result.getContents(), Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+}
